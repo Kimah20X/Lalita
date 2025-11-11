fc20ddc0b8a102ffa600423d895a56d31c630ff1
Kimah20X <pamlydia7@gmail.com>
2025-11-11 08:15:35 +0100

    commit

--- backend/scr/routes/savingsRoutes.js
+++ backend/scr/routes/savingsRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import {  depositFunds } from "../controllers/savingsController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { authorize } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // Protected route for deposits
-router.post("/deposit", protect, depositFunds);
+router.post("/deposit", authorize, depositFunds);
 
 export default router;
